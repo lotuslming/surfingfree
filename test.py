709805b5ec31c4661a75c3b67793abbf1d5d1ab9
lotuslming <35161448+lotuslming@users.noreply.github.com>
2018-08-05 23:14:04 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import pyodbc
 
-conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\wangmingle\Desktop\test.accdb;')
+conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\test\Desktop\test.accdb;')
 cursor=conn.cursor()
 cursor.execute('select * from table1')
 
