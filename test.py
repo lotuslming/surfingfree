d8482ca9a2d7dfa30904c92668a04a1c9cec6960
lotuslming <35161448+lotuslming@users.noreply.github.com>
2018-08-05 23:12:47 +0800

    test.py
    none

---
+++ test.py
@@ -0,0 +1,9 @@
+import pyodbc
+
+conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\wangmingle\Desktop\test.accdb;')
+cursor=conn.cursor()
+cursor.execute('select * from table1')
+
+for row in cursor.fetchall():
+    print(row)
+
